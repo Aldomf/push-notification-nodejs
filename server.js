d693ceda21821e78f3865cd39ab9498447a399ed
Aldo Miralles <alditomiralles3@gmail.com>
2024-08-16 21:23:27 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const admin = require('firebase-admin');
+const bodyParser = require('body-parser');
+const cors = require('cors'); // Import the cors package
+
+// Initialize Firebase Admin SDK with your service account
+const serviceAccount = require('./chat-react-ts-cf958-firebase-adminsdk-d6xum-cde0e520a2.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const app = express();
+// Use CORS middleware to enable CORS
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/send-notification', async (req, res) => {
+    const { token, message } = req.body;
+  
+    // Log the received data to check its type
+    console.log('Received token:', token);
+    console.log('Received message:', message);
+    console.log('Type of message:', typeof message);
+  
+    // Ensure the message is a string and not undefined
+    if (typeof message !== 'string' || !message) {
+      return res.status(400).send('Invalid message format');
+    }
+  
+    const payload = {
+      notification: {
+        title: 'New Message',
+        body: message,
+        icon: '/icon.png', // Specify your icon path
+      },
+    };
+  
+    try {
+      await admin.messaging().sendToDevice(token, payload);
+      res.status(200).send('Notification sent successfully');
+    } catch (error) {
+      console.error('Error sending notification:', error);
+      res.status(500).send('Failed to send notification');
+    }
+  });
+  
+  
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
